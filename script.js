3b3db60823622991aa36a125b943f8f67902546d
syedahoor <syedahumsagardazee@gmail.com>
2024-08-24 01:41:22 +0500

    check the string

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+// check of premitive types
+
+
 function type(para){
     if(typeof(para)=="string"){
         return "Yes you are string"
@@ -11,5 +14,108 @@ function type(para){
     }}
 
 console.log(type("edward"));
+
+
+
+
+// check the its string or not
+
+
+function array(num1, num2, num3, num4, num5) {
+    let array = []
+    for (let i = 0; i < 1; i++) {
+        if (typeof (num1) == "string") {
+            array.push(num1)
+
+        }
+
+        if (typeof (num2) == "string") {
+            array.push(num2)
+
+        }
+        if (typeof (num3) == "string") {
+            array.push(num3)
+
+        }
+        if (typeof (num4) == "string") {
+            array.push(num4)
+
+        } if (typeof (num5) == "string") {
+            array.push(num5)
+
+        }
+        console.log(num1);
+        
+    }
+    array("edward",3, "bella",9,"hoor")
+    console.log(array);
+
+}
+
+ 
+// function array(num1, num2, num3, num4, num5) {
+//     let array = []
+//     for (let i = 0; i < 1; i++) {
+//         if (typeof (num1) == "string") {
+//             array.push(num1)
+
+//         }
+
+//         if (typeof (num2) == "string") {
+//             array.push(num2)
+
+//         }
+//         if (typeof (num3) == "string") {
+//             array.push(num3)
+
+//         }
+//         if (typeof (num4) == "string") {
+//             array.push(num4)
+
+//         } if (typeof (num5) == "string") {
+//             array.push(num5)
+
+//         }
+//     }
+//     console.log(array);
+
+// }
+
+
+
+// array(1, 2, 3, 4, 5)
+
+
+// let star= "*"
+//  console.log(star);
+//  for (let i = 0; i < 5; i++) {
+//  console.log(i);
+    
+// }
+// console.log(star);
+
+// function star(num1){
+//     let array = []
+//         for (let i = 0; i < 5; i++) {
+
+// }
+//         }
+// star("*")
+
+// console.log(star.str);
+
+// //  table of 2
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} = ${2 * i}`);
+// }
+
+
+
+
+
+
+    
+
+
 
 
