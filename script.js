013647de4fecc345d81074fc699b367e8c28e826
syedahoor <syedahumsagardazee@gmail.com>
2024-08-24 01:26:05 +0500

    check types

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function type(para){
+    if(typeof(para)=="string"){
+        return "Yes you are string"
+    }else if(typeof(para)=="number"){
+        return "Yes you are number"
+    }else if (typeof(para)=="boolean"){
+        return "Yes you are boolean"
+    }else{
+        return "Invalid value"
+    }}
+
+console.log(type("edward"));
+
+
